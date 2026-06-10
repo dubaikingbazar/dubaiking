488d4737ba3dec296b4801f7523664d3c0e2ee78
dubaikingbazar <dubaikingbazar@gmail.com>
2026-06-11 00:02:52 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+
 const SUPABASE_URL = 'https://wejvwqncgapzzwvafjet.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_zCWmUf6Sb7g3iXbL1dQtZA_ztzB6uYg';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlanZ3cW5jZ2Fwenp3dmFmamV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMDYyMTAsImV4cCI6MjA5NjU4MjIxMH0.jCtqE-XJoWzbCdjBk2HfpfRifIypYkhPUXMNG_6HhT0';
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
